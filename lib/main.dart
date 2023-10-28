dbc9101084954cbc25d8bc25177d9b972e574481
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2023-10-28 23:22:57 +0500

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ void main (){
                 await player.play(UrlSource('assets/audio/town.mp3'));
 
               },
-              child: const Text('Play')),
+              child: const Text('')),
 
 
         ),Expanded(
